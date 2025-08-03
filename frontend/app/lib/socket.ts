8dc38199e385c6b934ef1d0ed8e3a9a70b60e05c
Kakarot <manjeetkumar1572000@gmail.com>
2025-08-03 21:07:09 +0530

    fixed errors

--- frontend/app/lib/socket.ts
+++ frontend/app/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api-v1";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const socket = io(BASE_URL, {
     autoConnect: false,
